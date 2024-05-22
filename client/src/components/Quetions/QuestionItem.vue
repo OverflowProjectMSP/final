b20f6b957c2518d1edaf519e1964744c0a8d2f0d
Garleen <garleenn@gmail.com>
2024-05-22 18:16:39 +0300

    ОАО ГАЗПРОМ

--- client/src/components/Quetions/QuestionItem.vue
+++ client/src/components/Quetions/QuestionItem.vue
@@ -144,7 +144,7 @@ export default {
 
 <template>
     <div class="container mb-4">
-        <div class="content-1">
+        <div class="content-1">+
             <div class="account justify-content-between">
                 <div class="creator-info d-flex flex-row align-items-center gap-3">
                     <img class="accountIcon" :src="userCreater.avatar" width="70px" alt="">
