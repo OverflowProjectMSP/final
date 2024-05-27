06acc5001437e0fa180f2b76eba414582cace2bc
Willyamdevon <dezhintern@gmail.com>
2024-05-27 17:03:56 +0300

    preparing for host app

--- server/app.py
+++ server/app.py
@@ -25,13 +25,18 @@ PASSWORD_PG = os.getenv('PASSWORD_PG')
 PORT_PG = os.getenv('PORT_PG')
 USER_PG = os.getenv('USER_PG')
 HOST_PG = os.getenv('HOST_PG')
+SECRET_KEY = os.getenv('SERCRET_KEY')
 
 app = Flask(__name__)
 
-app.secret_key = os.getenv('SECRET_KEY')
-app.permanent_session_lifetime = 60 * 60 * 24 * 28
-app.config["SESSION_COOKIE_SAMESITE"] = "None"
-app.config["SESSION_COOKIE_SECURE"] =  'None'
+app.secret_key = SECRET_KEY
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600 * 24
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+# app.config["SESSION_COOKIE_SECURE"] = "None"
+app.config.update(
+    SESSION_COOKIE_SECURE=False,
+    SESSION_COOKIE_HTTPONLY=True
+)
 
 # enable CORS
 CORS(app, resources={r"*": {"origins": "http://localhost:5173", 'supports_credentials': True}})
@@ -48,6 +53,92 @@ def home():
     session.pop('id', None)
     return jsonify(response_object)
 
+def add_tables():
+    try:
+        pg = psycopg2.connect(f"""
+                host={HOST_PG}
+                dbname=postgres
+                user={USER_PG}
+                password={PASSWORD_PG}
+                port={PORT_PG}
+            """)
+        cursor = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        cursor.execute(f"""create table if not exists users(
+                        id uuid,
+                        username text,
+                        email text,
+                        password text,
+                        name text,
+                        surname text,
+                        interestings text,
+                        about text,
+                        country text,
+                        region text,
+                        city text,
+                        telegram text,
+                        skype text,
+                        discord text,
+                        facebook text,
+                        phonenumber text,
+                        github text,
+                        avatar text,
+                        admin bool,
+                    )""")
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS questions(
+                    id uuid,
+                    descriptions text,
+                    details text,
+                    dificulty text,
+                    tag text,
+                    id_u uuid,
+                    data timestamp,
+                    is_solved bool
+                )""")
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS states(
+                    id uuid,
+                    descriptions text,
+                    details text,
+                    tag text,   
+                    id_u uuid,
+                    data timestamp
+                )""")
+        cursor.execute(f"""create table if not exists answers(
+                    id text,
+                    id_u text, 
+                    id_q text,
+                    text text,
+                    data timestamp
+                )""")
+        cursor.execute(f"""create table if not exists comments(
+                    id text,
+                    id_u text, 
+                    id_s text,
+                    text text,
+                    data timestamp
+                )""")
+        cursor.execute(f"""create table if not exists helper(
+                id uuid,
+                msg text,
+                phone text,
+                email text,
+                id_u uuid
+            )""")
+
+        pg.commit()
+    except (Exception, Error) as error:
+        logging.error(f'DB: ', error)
+        return_data = f"Error" 
+
+    finally:
+        if pg:
+            cursor.close
+            pg.close
+            logging.info("Соединение с PostgreSQL закрыто")
+            return return_data
+    
+
+    
 
 from user import * 
 from render_and_adding import *
