d95363c99ce570c798d04e57de9e6a2d09ccf0c4
Maxim <justhabber@gmail.com>
2024-06-02 21:11:25 +0300

    vk

--- client/src/components/ReuseComponets/FooterComp.vue
+++ client/src/components/ReuseComponets/FooterComp.vue
@@ -8,7 +8,7 @@
     <div class="d-flex flex-column align-items-center"> 
             <div class="d-flex flex-row">
                 <a href="#" style="--i:7"><img class="link user-select-none" src="../../assets/Footer/Telegram.png" alt="telegram"></a>
-                <a href="#" style="--i:8"><img class="link user-select-none" src="../../assets/Footer/Vk.png" alt="vk"></a>
+                <a href="https://vk.com/club226123105" style="--i:8"><img class="link user-select-none" src="../../assets/Footer/Vk.png" alt="vk"></a>
             </div>
             <p class="text">Upfollow</p>
             <hr class="hr">
