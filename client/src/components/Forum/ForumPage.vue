6691701092b336a84e267514d80a727257e945c0
Konstantin <151402518+Willyamdevon@users.noreply.github.com>
2024-06-03 18:26:12 +0300

    Update ForumPage.vue

--- client/src/components/Forum/ForumPage.vue
+++ client/src/components/Forum/ForumPage.vue
@@ -6,15 +6,15 @@ import axios from 'axios';
 <div class="container d-flex flex-column gap-2 align-items-center">
     <h2 class="text-center mt-3">Выберите язык:</h2>
     <a href="/Forum?lang=javascript"><button class="btn btn-outline-primary">JavaScript</button></a>
-    <a href="/Forum?lang=java"><button class="btn btn-outline-primary">Java</button></a>
-    <a href="/Forum?lang=cs"><button class="btn btn-outline-primary">C#</button></a>
     <a href="/Forum?lang=python"><button class="btn btn-outline-primary">Python</button></a>
-    <a href="/Forum?lang=php"><button class="btn btn-outline-primary">PHP</button></a>
-    <a href="/Forum?lang=cpp"><button class="btn btn-outline-primary">C++</button></a>
+    <a href="/Forum?lang=cs"><button class="btn btn-outline-primary">C#</button></a>
+    <a href="/Forum?lang=ts" class="a"><button class="btn btn-outline-primary">TypeScript</button></a>
     <a href="/Forum?lang=go"><button class="btn btn-outline-primary">Go</button></a>
-    <a href="/Forum?lang=ruby"><button class="btn btn-outline-primary">Ruby</button></a>
+    <a href="/Forum?lang=java"><button class="btn btn-outline-primary">Java</button></a>
+    <a href="/Forum?lang=cpp"><button class="btn btn-outline-primary">C++</button></a>
     <a href="/Forum?lang=kotlin"><button class="btn btn-outline-primary">Kotlin</button></a>
-  <a href="/Forum?lang=typescript" class="a"><button class="btn btn-outline-primary">TypeScript</button></a>
+    <a href="/Forum?lang=ruby"><button class="btn btn-outline-primary">Ruby</button></a>
+    <a href="/Forum?lang=php"><button class="btn btn-outline-primary">PHP</button></a>
 </div>  
 </template>
 
