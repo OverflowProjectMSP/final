4b85336c963f6c3601a28685419219a8e74d1d91
Konstantin <151402518+Willyamdevon@users.noreply.github.com>
2024-06-04 01:06:02 +0300

    Update ForumPage.vue

--- client/src/components/Forum/ForumPage.vue
+++ client/src/components/Forum/ForumPage.vue
@@ -8,7 +8,7 @@ import axios from 'axios';
     <a href="/Forum?lang=javascript"><button class="btn btn-outline-primary">JavaScript</button></a>
     <a href="/Forum?lang=python"><button class="btn btn-outline-primary">Python</button></a>
     <a href="/Forum?lang=cs"><button class="btn btn-outline-primary">C#</button></a>
-    <a href="/Forum?lang=ts" class="a"><button class="btn btn-outline-primary">TypeScript</button></a>
+    <a href="/Forum?lang=ts"><button class="btn btn-outline-primary">TypeScript</button></a>
     <a href="/Forum?lang=go"><button class="btn btn-outline-primary">Go</button></a>
     <a href="/Forum?lang=java"><button class="btn btn-outline-primary">Java</button></a>
     <a href="/Forum?lang=cpp"><button class="btn btn-outline-primary">C++</button></a>
