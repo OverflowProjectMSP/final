9f0d2809d2654cb40aa583002158791f3d295bef
Konstantin <151402518+Willyamdevon@users.noreply.github.com>
2024-07-08 02:14:58 +0300

    Update PageEnd.vue

--- client/src/components/Main/PageEnd.vue
+++ client/src/components/Main/PageEnd.vue
@@ -13,9 +13,9 @@ export default {
         <h3 class="join-us-title">Присоединяйся к нам!</h3>
         <p class="join-us-desc">Ведь у нас ты можешь:</p>
         <div class="join-us-items">
-            <a><div class="join-us-item"><button>Задавать вопросы</button></div></a>
-            <a><div class="join-us-item"><button>Помогать новичкам</button></div></a>
-            <a><div class="join-us-item"><button>Находить партнёров</button></div></a>
+            <a href="/Quetions"><div class="join-us-item"><button>Задавать вопросы</button></div></a>
+            <a href="/Quetions"><div class="join-us-item"><button>Помогать новичкам</button></div></a>
+            <a href="/Quetions"><div class="join-us-item"><button>Находить партнёров</button></div></a>
         </div>
     </div>
 </template>
