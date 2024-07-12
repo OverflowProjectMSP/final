cdfd8a39e790428e9856b36481e671ff3151591e
Willyamdevon <dezhintern@gmail.com>
2024-07-13 00:54:08 +0300

    small fixion

--- client/src/components/TestComp.vue
+++ client/src/components/TestComp.vue
@@ -1,123 +1,41 @@
+<template>
+  <div>
+    <textarea ref="textArea" v-model="text" @input="updateCursor" @click="updateCursor"></textarea>
+    <button @click="addBoldTag">B</button>
+  </div>
+</template>
+
 <script>
-import axios from "axios";
 export default {
   data() {
     return {
-      form: {
-        avatar: "",
-        formdata: null,
-      },
-      link: null,
-      formdata: null,
-      data: null,
-      isUploading: true,
-      percentCompleted: 0,
-      isLoading: false,
-      l: {
-        1: {
-          convert: "Файл незагружен.",
-          class: "notdownload",
-        },
-        2: {
-          convert: "Файл незагружен.",
-          class: "notdownload",
-        },
-      },
+      text: '',
+      cursorPosition: 0
     };
   },
   methods: {
-    async sendAvatar() {
-      this.isLoading = true;
-      this.isUploading = true;
-      try {
-        let res = await axios.post(
-          "/test",
-          this.formdata,
-          {
-            headers: {
-              "Content-Type": "enctype=multipart/form-data",
-            },
-            onUploadProgress: (progressEvent) => {
-              this.percentCompleted = Math.round(
-                (progressEvent.loaded * 100) / progressEvent.total
-              );
-            },
-          },
-          
-          
-        );
-        this.isLoading = true;
-        this.link = res.data.res;
-      } catch (err) {
-        console.log("Ошибка отправки данных", err);
-        this.isUploading = false;
-        return;
-        this.isLoading = false;
-      }
-
-      this.isUploading = false;
+    updateCursor(event) {
+      this.cursorPosition = event.target.selectionStart;
     },
-    async convertFileAvatar(event, name) {
-      const file = event.target.files[0];
-      this.form.avatar = file;
-      console.log(this.form.avatar);
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.form.avatar = e.target.result;
-        console.log(this.form.avatar);
-      };
+    addBoldTag() {
+      const beforeCursor = this.text.slice(0, this.cursorPosition);
+      const afterCursor = this.text.slice(this.cursorPosition);
 
-   
-
-      console.log(this.formdata);
-    },
-  },
-  mounted() {
-    console.log(this.l["1"].class);
-    this.formdata = new FormData();
-  },
+      this.text = `${beforeCursor}<b></b>${afterCursor}`;
+      this.$nextTick(() => {
+        const newCursorPosition = this.cursorPosition + 3; // Позиция курсора между тегами <b>|</b>
+        this.$refs.textArea.focus();
+        this.$refs.textArea.setSelectionRange(newCursorPosition, newCursorPosition);
+        this.updateCursor({target: this.$refs.textArea});
+      });
+    }
+  }
 };
 </script>
-<template>
-  {{ this.percentCompleted }}
-  {{ this.isLoading }}
-  {{ this.form.avatar }}
-  <form @submit.prevent="sendAvatar()">
-    <input
-      type="file"
-      class="form-control mt-5"
-      id="exampleFormControlInput1"
-      placeholder="name@example.com"
-      @change="convertFileAvatar($event, '1')"
-    />
-
-    <input
-      type="file"
-      class="form-control mt-5"
-      id="exampleFormControlInput1"
-      placeholder="name@example.com"
-      @change="convertFileAvatar($event, '2')"
-    />
-
-    <button type="submit" class="btn btn-primary mt-2">Отправить</button>
-    <a :href="this.link" class="link mt-5">{{ this.link }}</a>
-
-    <div class="mb-3" v-if="isLoading">
-      <label for="exampleFormControlInput1" class="form-label mt-3"
-        ><h5>Загрузка данных на сервер...</h5></label
-      >
-      <progress
-        max="100"
-        :value="this.percentCompleted"
-        v-if="this.isUploading"
-      ></progress>
-      <p>{{ this.percentCompleted }} %</p>
-    </div>
-  </form>
-</template>
 
 <style scoped>
-.link {
-  font-size: 50px;
+textarea {
+  width: 100%;
+  height: 100px;
 }
 </style>
