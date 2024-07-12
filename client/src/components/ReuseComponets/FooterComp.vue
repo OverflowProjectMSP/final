3c17f8fab32a22c0ffae9e8cfc0c276026bff088
Willyamdevon <dezhintern@gmail.com>
2024-07-13 02:50:44 +0300

    small fixion

--- client/src/components/ReuseComponets/FooterComp.vue
+++ client/src/components/ReuseComponets/FooterComp.vue
@@ -4,103 +4,111 @@
 
 <template>
 
-<div class="footer">
-    <div class="d-flex flex-column align-items-center"> 
-            <div class="d-flex flex-row">
-                <a href="https://t.me/upfollowru" style="--i:7"><img class="link user-select-none" src="../../assets/Footer/Telegram.png" alt="telegram"></a>
-                <a href="https://vk.com/club226123105" style="--i:8"><img class="link user-select-none" src="../../assets/Footer/Vk.png" alt="vk"></a>
-                                <a href="https://discord.gg/RmEbfBCs" style="--i:9"><img class="link user-select-none" src="../../assets/Footer/Discord.png" alt="discord"></a>
-            </div>
-            <p class="text">Upfollow</p>
-            <hr class="hr">
-            <p class="text_FAQ"><a href="/FAQ">FAQ</a></p>
-            <a href="#"><img class="arrow user-select-none" alt="arrows" src="../../assets/Footer/arrows.png"></a>
+  <div class="footer">
+    <div class="d-flex flex-column align-items-center">
+      <div class="d-flex flex-row">
+        <a href="https://t.me/upfollowru" style="--i:7" class="footer-icon"><img class="link user-select-none" src="../../assets/Footer/Telegram.png" alt="telegram"></a>
+        <a href="https://vk.com/club226123105" style="--i:8" class="footer-icon"><img class="link user-select-none" src="../../assets/Footer/Vk.png" alt="vk"></a>
+        <a href="https://discord.gg/RmEbfBCs" style="--i:9" class="footer-icon"><img class="link user-select-none" src="../../assets/Footer/Discord.png" alt="discord"></a>
+      </div>
+      <p class="text">Upfollow</p>
+      <hr class="hr">
+      <p class="text_FAQ"><a href="/FAQ">FAQ</a></p>
+      <a href="#"><img class="arrow user-select-none" alt="arrows" src="../../assets/Footer/arrows.png"></a>
     </div>
-</div>  
+  </div>
 </template>
 
 <style scoped>
 .arrow{
-    width: 41px;
-    height: 41px;
-    margin-top: 20px;
+  width: 41px;
+  height: 41px;
+  margin-top: 20px;
 }
 * {
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
 }
 a {
-    text-decoration: none;
-    color: #fff;
+  text-decoration: none;
+  color: #fff;
 }
-.footer{    
-    background-color: #3B82F6;
-    /* background: url('@/assets/Footer/back.png');  */
-    background-repeat: no-repeat;   
-    height: 300px;
-    width: 100%;
-    display: flex;
-    justify-content: center;      
-    align-content: center;  
-    flex-wrap: wrap;
-    
-    /* padding-top: 60px; */
-    /* border-radius: 10px 10px 0px 0px; */
+.footer{
+  background-color: #3B82F6;
+  /* background: url('@/assets/Footer/back.png');  */
+  background-repeat: no-repeat;
+  height: 300px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+
+  /* padding-top: 60px; */
+  /* border-radius: 10px 10px 0px 0px; */
 }
 .link {
-    width: 42px;
-    height: 42px;
-    margin-right: 5px;
-    padding: 8px;
-    animation: slideLeft 1s ease forwards;
-    animation-delay: calc(.1s * var(--i));
+  width: 42px;
+  height: 42px;
+  margin-right: 5px;
+  padding: 8px;
+  animation: slideLeft 1s ease forwards;
+  animation-delay: calc(.1s * var(--i));
 }
 .text{
-    margin: 8px 0;
-    font-size: 24px;
-    color: white;
+  margin: 8px 0;
+  font-size: 24px;
+  color: white;
 }
 .hr {
-    /* background: #000; */
-    height: 2px;
-    width: 100%;
-    color:white;
-    width: 200px;
+  /* background: #000; */
+  height: 2px;
+  width: 100%;
+  color:white;
+  width: 200px;
 }
 .logo{
-    text-align: center;
-    margin: 10px 0px;
-    color: white;
-    font-size: 24px;
+  text-align: center;
+  margin: 10px 0px;
+  color: white;
+  font-size: 24px;
 }
 .text_FAQ{
-    margin: 8px 0;
-    font-size: 24px;
-    color: white;
-    text-align: center;
+  margin: 8px 0;
+  font-size: 24px;
+  color: white;
+  text-align: center;
 }
 
+.footer-icon{
+  transition:  all 300ms;
+}
 
+@media (hover: hover) {
+  .footer-icon:hover {
+    transform: translateY(-8px)
+  }
+}
 
 
 @media (max-width: 400px) {
-    .footer{
-        padding-top: 30px;
-    }
-    
+  .footer{
+    padding-top: 30px;
+  }
+
 }
 
 /* KEYFRAMES ANIMATION */
 @keyframes slideLeft {
-    0% {
-        transform: translateX(100px);
-        opacity: 0;
-    }
+  0% {
+    transform: translateX(100px);
+    opacity: 0;
+  }
 
-    100% {
-        transform: translateX(0);
-        opacity: 1;
-    }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
 }
 </style>
