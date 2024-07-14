4778dda31e015ed379c34fd685d8b6d147a6b4cb
kinestrik <shadlovskiyy@bk.ru>
2024-07-14 19:03:26 +0300

    e

--- client/src/components/TestComp.vue
+++ client/src/components/TestComp.vue
@@ -13,6 +13,8 @@ export default {
       isUploading: true,
       percentCompleted: 0,
       isLoading: false,
+      err: '',
+      arr: [],
       l: {
         1: {
           convert: "Файл незагружен.",
@@ -29,6 +31,7 @@ export default {
     async sendAvatar() {
       this.isLoading = true;
       this.isUploading = true;
+      this.arr.push(this.formdata)
       try {
         let res = await axios.post(
           "/test",
@@ -51,8 +54,9 @@ export default {
       } catch (err) {
         console.log("Ошибка отправки данных", err);
         this.isUploading = false;
-        return;
         this.isLoading = false;
+        this.error = 'Ошибка отправки данных.'
+        return;
       }
 
       this.isUploading = false;
@@ -60,14 +64,11 @@ export default {
     async convertFileAvatar(event, name) {
       const file = event.target.files[0];
       this.form.avatar = file;
-      console.log(this.form.avatar);
       const reader = new FileReader();
+      this.formdata.append(name, file);
       reader.onload = (e) => {
         this.form.avatar = e.target.result;
-        console.log(this.form.avatar);
-      };
-
-   
+        console.log(this.form.avatar);      }
 
       console.log(this.formdata);
     },
@@ -101,7 +102,7 @@ export default {
 
     <button type="submit" class="btn btn-primary mt-2">Отправить</button>
     <a :href="this.link" class="link mt-5">{{ this.link }}</a>
-
+{{ this.arr }}
     <div class="mb-3" v-if="isLoading">
       <label for="exampleFormControlInput1" class="form-label mt-3"
         ><h5>Загрузка данных на сервер...</h5></label
@@ -112,7 +113,9 @@ export default {
         v-if="this.isUploading"
       ></progress>
       <p>{{ this.percentCompleted }} %</p>
+       
     </div>
+    {{ this.error }}
   </form>
 </template>
 
