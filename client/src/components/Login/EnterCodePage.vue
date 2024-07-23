19fee5ed70e6b4d2ef99da532a91f9e5a11aee20
kinestrik <shadlovskiyy@bk.ru>
2024-07-23 11:11:59 +0300

    EnterCode is ready

--- client/src/components/Login/EnterCodePage.vue
+++ client/src/components/Login/EnterCodePage.vue
@@ -66,228 +66,525 @@ export default {
 
 </script>
 
+
 <template>
-  <div class="tototocontainer">
-    <div class="background"></div>
-    <div class="content">
-      <p>Введите код доступа</p>
-      <p class="toemail">На почту  {{ email }} вам был отправлен ключ доступа </p>
-      <div class="regist">
-        <form @submit.prevent="check">
-          <div class="kostya_zahotel_po_gorizontali">
-            <input class="numinp form-control" type="text" v-model="CIFRY">
-            <button @click="check">Подтвердить</button>
-          </div>
-          <p v-if=this.error class="error">{{ error }}</p>
-        </form>
+  <div class="all-container">
+    <h6>UF</h6>
+    <div class="login-container container">
+      <div class="img-container">
+        <img src="../../assets/Login/mounted.png" alt="" class="img" />
+      </div>
+      <div class="head mt-4">
+        <h1>Код подтверждения</h1>
+        <p>На вашу почту был направлен код подтверждения</p>
+      </div>
+      <form @submit.prevent="checkCode">
+      <div class="input-container">
+        <div class="div-nickname">
+          <input type="text" v-model="CIFRY" class="input" auыtofocus required />
+          <span сlass="nick">Код</span>
+        </div>
+        <div class="btn-container">
+          <button class="bt login" type="submit"><img src="../../assets/Login/23.png" alt=""></button>
+        </div>
+      </div>
+    </form>
+      <div class="mail">
+        <a href=""><img src="/mail.svg" alt="" /></a>
+      </div>
+      <div class="pass-err">
+        <a href="" class="a mt-3">Не приходит код?</a>
+        <div class="errors" v-if="error"><p class="err">{{error}}</p></div>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-.tototocontainer {
-  width: 100%;
+<style scoped>
+:root {
+  --font-family: 'Rubik', sans-serif;
+  --second-family: 'Inter', sans-serif;
+}
+.err {
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 20px;
+  color: #f00;
+  position: absolute;
+  top: 550px;
+  left: 530px;
+}
+.a {
+  position: absolute;
+  top: 350px;
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 20px;
+  color: #3b82f6;
+  text-decoration: none;
+  z-index: 8000;
+  left: 530px;
+}
+.mail {
+  position: relative;
+}
+h6 {
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 25px;
+  color: #3b82f6;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  display: block;
+}
+.img-container {
+  display: block;
+}
+.mail img {
+  position: absolute;
+  top: 550px;
+  cursor: pointer;
+  right: 10px;
+}
+.all-container {
   display: flex;
-}
-
-.background {
-  width: 40%;
-  height: 92vh;
-  background: url(../../assets/Login/backgroundUp.png) center;
-  background-repeat: no-repeat;
-}
-
-.content p {
-  font-size: 42px;
-  margin-bottom: 12px;
-}
-
-.content {
-  margin-top: 219px;
-  margin-left: 200px;
-}
-
-.regist form {
-
-  flex-direction: row;
-  gap: 20px;
-  margin-bottom: 40px;
-}
-
-.toemail {
-  font-size: 20px !important;
-  color: #A4A4A4;
-}
-
-input[type=number]::-webkit-inner-spin-button,
-input[type=number]::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-  margin: 0; /* Удалить отступы */
-}
-
-.numinp {
-  padding-left: 5px;
-
-  width: 280px;
-  height: 80px;
-  font-size: 80px;
-  border-radius: 10px !important;
-  border: 1px solid #121212 !important;
-}
-
-.regist button {
-  /* margin-top: 40px; */
-  /* margin-bottom: 15px; */
-
-  width: 220px;
-  height: 50px;
-  font-size: 24px;
-  background: none;
-
-  border: 1px solid #2D72D9;
-  border-radius: 8px;
-  color: #2D72D9;
-
-  transition: all 100ms;
-}
-
-.regist button:active {
-  background-color: #2D72D9;
-  color: #fff;
-}
-
-
-.regist p {
-  font-size: 26px;
-  margin: 0;
-}
-
-.regist a {
-  font-size: 26px;
-  color: #2D72D9;
-  font-weight: 500;
-}
-
-.regist a:hover {
-  color: #1b54a9;
-}
-
-.regist a:active {
-  color: #114189;
-}
-
-.error {
-  font-size: 18px !important;
-  color: #ff1f1f;
-  margin-bottom: -30px !important;
-  margin-top: -30px !important;
-}
-
-
-.kostya_zahotel_po_gorizontali {
-  display: flex;
-  gap: 40px;
+  background-color: #ecedee;
   align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+}
+.login-container {
+  display: flex;
+  border-radius: 50px;
+  margin: 80px;
+  width: 1100px;
+  height: 620px;
+  background: #fff;
+  position: relative;
+}
+.img {
+  height: 620px;
+  transform: translateX(-50px);
+  transition: all 0.3s;
+}
+.head {
+  height: 50px;
+}
+.head h1 {
+    font-size: 40px;
+    margin-bottom: 20px;
+}
+.head p {
+    font-size: 30px;
+    font-weight: bold;
+    color: #5B5A5A;
+    margin-left: 40px;
+    margin-bottom: 20px;
+}
+.password {
+  /* display: flex; */
+  /* align-items: center; */
+  position: relative;
+  width: 30px;
+}
+.password span {
+  position: absolute;
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 25px;
+  color: #5b5a5a;
+  top: 14px;
+  left: 18px;
+  pointer-events: none;
+  transition: 0.3s ease;
+  border-radius: 15px;
+}
+h1 {
+  font-weight: 700;
+  font-size: 80px;
+  color: #000;
+  width: 100%;
+  margin: 80px 0px 0px 40px;
+}
+.input-container {
+  display: flex; /* Добавляем flexbox */
+  flex-direction: column; /* Устанавливаем вертикальное направление */
+  margin: 290px 0px 0px;/* Меняем отступ */
+  position: absolute;
+  left: 525px;
+  width: 400px; /* Добавляем ширину для блока инпутов */
+  z-index: 300;
+}
+
+.input {
+  margin-bottom: 40px;
+  border-radius: 15px;
+  width: 430px;
+  height: 60px;
+  background: #eae9e9;
+  border: none;
+  padding: 20px;
+  font-family: var(--font-family);
+  font-weight: 500;
+  font-size: 20px;
+  color: #000000;
+}
+.btn-container {
+  position: absolute;
+  top: 0;
+  left: 450px;
+  display: flex;
+  justify-content: center; /* Выравниваем кнопки по центру */
+  /* margin-top: -10px;  */
+  gap: 30px;
+  /* margin-left: -250px; */
+}
+
+.bt {
+  border-radius: 15px;
+  width: 60px;
+  height: 60px;
+  background: blue;
+  border: none;
+  cursor: pointer;
+  z-index: 7000;
+}
+.reg {
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 25px;
+  text-align: center;
+  color: #5b5a5a;
+  background: #eae9e9;
+  cursor: pointer;
+}
+
+.login {
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 25px;
+  text-align: center;
+  color: #fff;
+  background: #3b82f6;
+  text-align: center;
+  cursor: pointer;
+}
+.login img {
+  width: 30px;
+}
+.eye {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  z-index: 400;
+  cursor: pointer;
+  top: 7px;
+  right: -380px;
+}
+.div-nickname {
+  position: relative;
+  width: 300px;
+}
+.div-nickname span {
+  position: absolute;
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 25px;
+  color: #5b5a5a;
+  top: 14px;
+  left: 18px;
+  pointer-events: none;
+  transition: 0.3s ease;
+  border-radius: 15px;
+  width: 115px;
+}
+.reg:hover {
+  color: #5b5a5a;
+  background: #d7d5d5;
+  transition: all 0.3s;
+}
+.input:focus {
+  outline: none;
+  border: none;
+  border: 4px solid #3b82f6;
+  transition: all 0.1s;
+}
+.login:hover {
+  color: #fff;
+  background: #2e77ed;
+}
+.input:focus ~ span,
+.input:valid ~ span {
+  transform: translateY(-115%);
+  font-size: 20px;
+  left: 25px;
+  border: solid #3b82f6;
+  background: #3b82f6;
+  color: #fff;
+  width: 110px;
+  text-align: center;
+  border-radius: 15px;
+}
+@media (max-width: 1260px) {
+  /* .mail img {
+    position: absolute;
+    top: 550px;
+    cursor: pointer;
+    right: -110px;
+  } */
+
+  .btn-container {
+    position: absolute;
+    top: 80px;
+    left: 0;
+  }
+
+  .a {
+    top: 420px;
+  }
+
+  
+}
+@media (max-width: 1235px) {
+  /* .mail img {
+    position: absolute;
+    top: 550px;
+    cursor: pointer;
+    right: -70px;
+  } */
+}
+
+@media (max-width: 1199px) {
+  .mail img {
+    position: absolute;
+    top: 550px;
+    cursor: pointer;
+    right: 30px;
+  }
+
+  .input {
+    width: 360px;
+  }
 
 }
 
-@media (hover: hover) {
-  .regist button:hover {
-    background-color: #2D72D9;
-    color: #fff;
+@media (max-width: 1175px) {
+  .img-container {
+    display: none;
   }
 
-  .regist button:active {
-    background-color: #1b54a9;
+  .head h1 {
+    font-size: 30px;
   }
 
-}
-
-@media (max-width: 1330px) {
-  .content {
-    margin-left: 50px;
-  }
-}
-
-
-@media (max-width: 1140px) {
-  .content p {
-    font-size: 38px;
+  .head p {
+    font-size: 30px;
   }
 
-  .inp {
-    width: 550px;
+  .a {
+    position: absolute;
+    top: 370px;
+    left: 50px;
   }
-
-  .haveacc p {
-    font-size: 22px;
+  .mail img {
+    position: absolute;
+    top: 550px;
+    cursor: pointer;
+    right: 10px;
   }
-
-  .haveacc a {
-    font-size: 22px;
-  }
-}
-
-@media (max-width: 1000px) {
-  .kostya_zahotel_po_gorizontali {
-    gap: 20px;
-  }
-}
-
-@media (max-width: 960px) {
-  .kostya_zahotel_po_gorizontali {
-    flex-direction: column;
-    align-items: flex-start;
-  }
-}
-
-@media (max-width: 830px) {
-  .background {
-    background: none;
-    width: 0%;
-  }
-
-  .tototocontainer {
-    justify-content: center;
-  }
-
-  .content {
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-
-  .inp {
-    width: 550px;
-  }
-}
-
-@media(max-width: 600px) {
-  .content {
+  .login-container {
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    border-radius: 50px;
+    margin: 80px;
+    width: 550px;
+    height: 600px;
+    background: #fff;
+    position: relative;
+  }
+  .err {
+    width: 1000px;
+    position: absolute;
+    top: 550px;
+    margin-left: -479px
+  }
+  
+  .input-container {
+    top: 0px;
+    left: 50px;
   }
 
-  .inp {
-    width: 350px;
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  .regist button {
-    width: 100%;
+  .btn-container {
+    top: 0;
+    left: 400px;
   }
 }
-
-@media (max-width: 440px) {
-  .haveacc {
-    flex-direction: column
+@media (max-width: 705px) {
+  /* .mail img {
+    position: absolute;
+    top: 550px;
+    cursor: pointer;
+    right: 20px;
+  } */
+}
+@media (max-width: 685px) {
+  .mail img {
+    position: absolute;
+    top: 550px;
+    cursor: pointer;
+    right: 50px;
+  }
+  
+  .head h1 {
+    font-size: 25px;
   }
 
-  .content p {
+  .head p {
+    font-size: 25px;
+  }
+
+  .a {
+    position: absolute;
+    left: 250px;
+    top: 350px
+  }
+  .input {
+    margin-bottom: 40px;
+    border-radius: 15px;
+    width: 330px;
+    height: 60px;
+    background: #eae9e9;
+    border: none;
+    padding: 20px;
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 20px;
+    color: #000000;
+  }
+
+  .input-container {
+    top: 0px;
+    
+  }
+
+  .input {
+    width: 300px;
+  }
+
+  .bt {
+    border-radius: 15px;
+    width: 60px;
+    height: 60px;
+    border: none;
+    cursor: pointer;
+    z-index: 7000;
+  }
+
+  .btn-container {
+    top: 80px;
+    left: 0;
+  }
+  .login-container {
+    display: flex;
+    border-radius: 50px;
+    margin: 80px;
+    width: 430px;
+    height: 600px;
+    background: #fff;
+    position: relative;
+  }
+  .eye {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    z-index: 100;
+    cursor: pointer;
+    top: 7px;
+    right: -280px;
+  }
+  .reg {
+    font-family: var(--font-family);
+    margin-left: -70px;
+    font-weight: 700;
+    font-size: 20px;
     text-align: center;
+    color: #5b5a5a;
+    background: #eae9e9;
+    cursor: pointer;
+  }
+  .login {
+    font-family: var(--font-family);
+    font-weight: 700;
+    font-size: 20px;
+    text-align: center;
+    color: #fff;
+    background: #3b82f6;
+    text-align: center;
+    cursor: pointer;
   }
 }
 
+@media (max-width: 567px) {
+  .input {
+    margin-bottom: 40px;
+    border-radius: 15px;
+    width: 300px;
+    height: 60px;
+    background: #eae9e9;
+    border: none;
+    padding: 20px;
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 17px;
+    color: #000000;
+  }
+  .eye {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    z-index: 100;
+    cursor: pointer;
+    top: 7px;
+    right: -250px;
+  }
+  .bt {
+    border-radius: 15px;
+    width: 135px;
+    height: 60px;
+    border: none;
+    cursor: pointer;
+    z-index: 7000;
+  }
+  .login {
+    font-family: var(--font-family);
+    font-weight: 700;
+    font-size: 20px;
+    margin-right: 30px;
+  }
+  .mail img {
+    right: 20px;
+  }
+  h6 {
+    display: none;
+  }
+  .reg {
+    font-family: var(--font-family);
+    font-weight: 700;
+    font-size: 18px;
+  }
+}
+@media (max-width: 505px) {
+  .all-container {
+    background: #fff;
+  }
+  /* .login-container {
+    -webkit-box-shadow: 4px 4px 100px 0px rgba(34, 60, 80, 0.2);
+    -moz-box-shadow: 4px 4px 100px 0px rgba(34, 60, 80, 0.2);
+    box-shadow: 4px 4px 100px 0px rgba(34, 60, 80, 0.2);
+
+    margin-left: 50px;
+  } */
+}
 </style>
